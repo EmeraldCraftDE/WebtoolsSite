018940f96ccaea4e60b2f51750142e3e30a2edbe
Ben "thekumpel <thekumpelhd@gmail.com>
2016-06-07 17:07:58 +0200

    Add files via upload

---
+++ scripts/index.php
@@ -0,0 +1,4 @@
+<title> WebTools - Scripts! </title>
+<h1> Hier gibt es kostenlose Scripts! </h1>
+<h1> Der Blaue hintergrund: </h1>
+<iframe src="bcg.txt"></iframe>
---
+++ user.php
@@ -0,0 +1,20 @@
+
+<script src="http://www.nightmc.net/htmlguide/guide/js/bootstrap.min.js"></script>
+<script src="http://www.nightmc.net/htmlguide/guide/js/jquery.js"></script>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<link href="http://www.nightmc.net/htmlguide/guide/css/bootstrap.min.css" rel="stylesheet">
+<script src="http://vps.nightmc.net/background.js"></script>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="http://webtools.esy.es">WebTools</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+      <li class="active"><a href="user.php">User</a></li>
+  </div>
+</nav>
+<h1> WebTools User-Management Not working yet! </h1>
